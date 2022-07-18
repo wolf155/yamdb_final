922ebab73e026fe5443b8b3d6ad1328a39ed312f
alex <you@example.com>
2022-07-18 20:13:40 +0300

    v1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -19,9 +19,9 @@ from reviews.models import Category, Genre, Review, Title, User
 
 from api_yamdb.settings import ADMIN_EMAIL
 
-from .permissions import (CreateIsAdmin, IsAdmin, IsAdminOrReadOnly,
-                          IsModerator, IsOwnerOrReadOnly,
-                          IsSuperUser, IsUser)
+from .permissions import (
+    CreateIsAdmin, IsAdmin, IsAdminOrReadOnly,
+    IsModerator, IsOwnerOrReadOnly, IsSuperUser, IsUser)
 
 
 class UserViewSet(viewsets.ModelViewSet):

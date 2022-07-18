23273e615ff7892323833a7fa38958493ddaa6b8
alex <you@example.com>
2022-07-18 20:07:58 +0300

    v1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -19,7 +19,7 @@ from reviews.models import Category, Genre, Review, Title, User
 
 from api_yamdb.settings import ADMIN_EMAIL
 
-from .permissions import (CreateIsAdmin, IsAdminOrReadOnly, IsAdmin,
+from .permissions import (CreateIsAdmin, IsAdmin, IsAdminOrReadOnly,
                           IsModerator, IsOwnerOrReadOnly,
                           IsSuperUser, IsUser)
 
